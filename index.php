abc9bc74ab36b4fa7d919a8c87a793fae65a5243
AlbertoLorente92 <alberto.lorente92@gmail.com>
2016-02-02 16:02:41 +0100

    Bootstrap and new desing

--- index.php
+++ index.php
@@ -1,162 +1,166 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Alberto Lorente. Software Engineer</title>
-	<link href="css/style.css" rel="stylesheet" type="text/css" />
-	<script type="text/javascript" src="js/javascript.js"></script>
-	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-	<link rel="shortcut icon" href="img/icon.png" type="image/png">
-</head>
-<body onload="javascript:window_onload();">
-	<header class="header" id="home">	
-		<!-- NAVIGATION LINKS -->
-		<div class="navbar_absolute" id="menu">
-			<br/>
-			<table>
-				<tr>
-					<td>
-						<a class="navbar" href="#"><img src="/img/ALlogo.png" alt="Logo AlbertoLorente" width="400" height="65"></a>
-					</td>
-					<td id="entryMenu">
-						<a href="#home">Home</a>
-					</td>
-					<td id="entryMenu">
-						<a href="#studies">Studies</a>
-					</td>
-					<td id="entryMenu">
-						<a href="#workExperience">Work Experience</a>
-					</td>
-					<td id="entryMenu">
-						<a href="#recommendations">Recommendations</a>
-					</td>
-					<td id="entryMenu">
-						<a href="#jobs">Jobs</a>
-					</td>
-					<td id="entryMenu">
-						<a href="#personal">Personal</a>
-					</td>
-				</tr>
-			</table>
-		</div>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="img/icon2.png">
+
+    <title>Alberto Lorente. Software Engineer</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/theme.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body role="document">
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a  href="#"><img src="img/new.png" alt="AlbertoLorente" border="2" align="middle" width="300px" height="50px"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#studies">Studies</a></li>
+            <li><a href="#workExperience">Work Experience</a></li>
+            <li><a href="#recommendations">Recommendations</a></li>
+			<li><a href="#jobs">Jobs</a></li>
+			<li><a href="#personal">Personal</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container theme-showcase" role="main">
+
+      <!-- Main jumbotron for a primary marketing message or call to action -->
+      <div class="jumbotron">
+	    <table>
+			<tr>
+				<td class="intro">
+					<h1>Software Engineer</h1>
+					<p>This page is a personal and professional blog about me where I expose my work and thoughts to the network</p>
+					<p>Work in progres!</p>
+				</td>
+				<td class="intro" style="text-align:center;">
+					<img src="img/photo2.jpg" alt="AlbertoLorente" border="2">
+				</td>
+			</tr>
+		</table>
+      </div>
+
+      <div id="studies" class="page-header">
+	    <br/><br/><br/>
+        <h1>Studies</h1>
+      </div>
+	  	<p><strong>Master in Computer Engineering</strong> Complutense University. Madrid. Now.</p>
+		<p><strong>Software Engineering's Degree.</strong> Complutense University. Madrid. 2015. Click  <a href="https://drive.google.com/open?id=0B1MxBhOQaDc-S3pUTWE4cmVhYm8" target="_blank">here</a> to see my accademic record.</p>
+		<p><strong>Technician Airport Operations (TAO).</strong> College of Aeronautics. Madrid. 2011.</p>
+		<p><strong>Bachelor's Degree.</strong> San Saturio School. Madrid. 2010.</p>
+      
+      <div id="workExperience" class="page-header">
+	    <br/><br/><br/>
+        <h1>Work Experience</h1>
+      </div>
+		<p><strong>Computer Scholar.</strong> Law School (UCM). Madrid. Now.</p>
+		<p>Working with the Dean of Innovation and Quality maintaining the website of the School in addition to the other masters and titles.</p>
+		<p><strong>Computer Scholar.</strong> Faculty of Statistical Studies (UCM). Madrid. 2014.</p>
+		<p>Working in the Operations Research department assists in maintaining (Linux, Windows) systems in networks and equipment; management of databases My SQL and Access; troubleshooting.</p>
+		<p><strong>Tutor</strong> Mathematics, Physics, Technical Drawing. Madrid. 2009.</p>
+	  
+	  <div id="recommendations" class="page-header">
+	    <br/><br/><br/>
+        <h1>Recommendations</h1>
+      </div>
+		<p>My little professional experience compensates with a high degree of satisfaction of my superiors at work.</p>
+		<p>In my current job I have been recommended for the award of excellence Deloitte two great professionals.</p>
+		<p><strong>Pilar Peiteado Mariscal.</strong> Dean of Innovation and Quality of Law School (UCM). Click <a href="img/pilar.jpg" target="_blank">here</a> to see it (spanish).</p>
+		<p><strong>Alfredo Mu&ntilde;oz Garcia.</strong> Director of Master of Access to the Profession of Lawyer. Click <a href="img/alfredo.jpg" target="_blank">here</a> to see it (spanish).</p>
+		<p></p>
+		<p>In my work as Computer Scholar at the Faculty of Statistics I didn't request a letter of recommendation. However I validate my work as 
+		External Practices where i get an outstanding mark.</p>
+	  
+	  <div id="jobs" class="page-header">
+	    <br/><br/><br/>
+        <h1>Jobs</h1>
+      </div>
+		<p>During the Degree most work, both individually and in groups I keep them in Dropbox. I am currently collecting 
+		these works and moving them to <a href="https://github.com/AlbertoLorente92" target="_blank">GitHub</a>.</p>
+		<p>Currently in the master I'm also doing work with private visibility. I will liberate it as soon as I finish the Master.</p>
+		<p>If you want to see my final degree work  you could do it <a href="tfg.php">here</a>.</p>
+		<p>All these works helped me to learn many languages like: </p>
+		<ul>
+			<li>Java</li>
+			<li>C and C++</li>
+			<li>HTML, JavaScript, CSS and PHP</li>
+			<li>Database MySQL and Oracle</li>
+			<li>Python</li>
+			<li>Maude</li>
+			<li>MongoDB</li>
+		</ul>
+		<p>The last three of them currently in learning the Master.</p>
 		
-		<div class="blueBackG" id="ini">
-			<br/><br/><br/>	<br/><br/>
-			<table>
-				<tr>
-					<td class="intro">
-						<h1>Software Engineer</h1>
-						<h5>This page is a personal and professional blog about me where I expose my work and thoughts to the network</h5>
-						<h5>Work in progres!</h5>
-					</td>
-					<td class="intro" style="text-align:center;">
-						<img src="/img/photo2.jpg" alt="AlbertoLorente" border="2" align="middle">
-					</td>
-				</tr>
-			</table>
-		</div>
-	</header>
-	<section class="whiteBackG" id="studies">
-		<div class="textCenter">  
-			<br/><br/><br/><br/><br/>
-			<h1>Studies</h1>
-			<p><strong>Master in Computer Engineering</strong> Complutense University. Madrid. Now.</p>
-			<p><strong>Software Engineering's Degree.</strong> Complutense University. Madrid. 2015. Click  <a href="https://drive.google.com/open?id=0B1MxBhOQaDc-S3pUTWE4cmVhYm8" target="_blank">here</a> to see my accademic record.</p>
-			<p><strong>Technician Airport Operations (TAO).</strong> College of Aeronautics. Madrid. 2011.</p>
-			<p><strong>Bachelor's Degree.</strong> San Saturio School. Madrid. 2010.</p>
-		</div>
-	</section>
-	
-	<section class="grayBackG" id="workExperience">
-		<div class="textCenter">  
-			<br/><br/><br/>	<br/><br/>
-			<h1>Work Experience</h1>
-			<p><strong>Computer Scholar.</strong> Law School (UCM). Madrid. Now.</p>
-			<p>Working with the Dean of Innovation and Quality maintaining the website of the School in addition to the other masters and titles.</p>
-			<p><strong>Computer Scholar.</strong> Faculty of Statistical Studies (UCM). Madrid. 2014.</p>
-			<p>Working in the Operations Research department assists in maintaining (Linux, Windows) systems in networks and equipment; management of databases My SQL and Access; troubleshooting.</p>
-			<p><strong>Tutor</strong> Mathematics, Physics, Technical Drawing. Madrid. 2009.</p>
-		</div>
-	</section>
+		
+	  <div id="personal" class="page-header">
+	    <br/><br/><br/>
+        <h1>Personal</h1>
+      </div>
+		<p>At the end of the third degree course I started working at the Faculty of Statistics. At the same time economic problems in the 
+		family began to appear. So the last year I started to pay me the university.</p>
+		<p>My work overlaps with my studies stopping going to school most of the time. Still I passed all the first quarter subjects some of them with high marks.</p>
+		<p>Now I work in law school in the morning and go to master the afternoon. Studying and doing practical when and where I can.</p>
+		<p>There's not much to say except the typical phrases (spanish); "es por tu futuro" (it's for your future)...</p>
+		<p>But I prefer other better, Work Hard and never give up!</p>
+	    <br/><br/><br/>		
+	  <footer class="blog-footer">
+		<a href="mailto:alberto.lorente92@gmail.com"><img src="img/gmail.png" alt="Gmail alberto.lorente92@gmail.com" width="100" height="100"></a>
+		<a href="https://github.com/AlbertoLorente92" target="_blank"><img src="img/github.png" alt="GitHub AlbertoLorente92" width="100" height="100"></a>
+	    <p>
+	      <a href="#">Back to top</a>
+	    </p>
+	  </footer>
+		
 
-	<section class="whiteBackG" id="recommendations">
-		<div class="textCenter">  
-			<br/><br/><br/>	<br/><br/>
-			<h1>Recommendations</h1>
-			<p>My little professional experience compensates with a high degree of satisfaction of my superiors at work.</p>
-			<p>In my current job I have been recommended for the award of excellence Deloitte two great professionals.</p>
-			<p><strong>Pilar Peiteado Mariscal.</strong> Dean of Innovation and Quality of Law School (UCM). Click <a href="/img/pilar.jpg" target="_blank">here</a> to see it (spanish).</p>
-			<p><strong>Alfredo Mu&ntilde;oz Garcia.</strong> Director of Master of Access to the Profession of Lawyer. Click <a href="/img/alfredo.jpg" target="_blank">here</a> to see it (spanish).</p>
-			<p></p>
-			<p>In my work as Computer Scholar at the Faculty of Statistics I didn't request a letter of recommendation. However I validate my work as 
-			External Practices where i get an outstanding mark.</p>
-		</div>
-	</section>
-	
-	<section class="grayBackG" id="jobs">
-		<div class="textCenter">  
-			<br/><br/><br/>	<br/><br/>
-			<h1>Jobs</h1>
-			<p>During the Degree most work, both individually and in groups I keep them in Dropbox. I am currently collecting 
-			these works and moving them to <a href="https://github.com/AlbertoLorente92" target="_blank">GitHub</a>.</p>
-			<p>Currently in the master I'm also doing work with private visibility. I will liberate it as soon as I finish the Master.</p>
-			<p>If you want to see my final degree work  you could do it <a href="tfg.php">here</a>.</p>
-			<p>All these works helped me to learn many languages like: </p>
-			<ul>
-				<li>Java</li>
-				<li>C and C++</li>
-				<li>HTML, JavaScript, CSS and PHP</li>
-				<li>Database MySQL and Oracle</li>
-				<li>Python</li>
-				<li>Maude</li>
-				<li>MongoDB</li>
-			</ul>
-			<p>The last three of them currently in learning the Master.</p>
-		</div>
-	</section>
-	
-	<section class="whiteBackG" id="personal">
-		<div class="textCenter">  
-			<br/><br/><br/>	<br/><br/>
-			<h1>Personal</h1>
-			<p>At the end of the third degree course I started working at the Faculty of Statistics. At the same time economic problems in the 
-			family began to appear. So the last year I started to pay me the university.</p>
-			<p>My work overlaps with my studies stopping going to school most of the time. Still I passed all the first quarter subjects some of them with high marks.</p>
-			<p>Now I work in law school in the morning and go to master the afternoon. Studying and doing practical when and where I can.</p>
-			<p>There's not much to say except the typical phrases (spanish); "es por tu futuro" (it's for your future)...</p>
-			<p>But I prefer other better, Work Hard and never give up!</p>
-		</div>
-	</section>
-	
-	<section class="blueEnd" id="social">
-		<div class="blueEnd">
-			<br/>
-			<table>
-				<tr>
-					<td>
-						<a href="mailto:alberto.lorente92@gmail.com"><img src="/img/gmail.jpg" alt="Gmail alberto.lorente92@gmail.com" width="100" height="100"></a>
-					</td>
-					<td>
-						<a href="https://github.com/AlbertoLorente92" target="_blank"><img src="/img/github.png" alt="GitHub AlbertoLorente92" width="100" height="100"></a>
-					</td>
-				</tr>
-			</table>
-			<br/>
-		</div>
-	</section>
 
-	<section class="grayEnd" id="agreement">
-		<div class="">  
-			<p align="center"><a class="navbar" href="#"><img src="/img/ALlogo.png" alt="Logo AlbertoLorente" width="400" height="65" align="middle"></a></p>
-		</div>
-	</section>
-	
-	<section class="blueEnd" id="foot">
-		<div class="">  
-			<br/>
-			<h5 align="center">Alberto Lorente S&aacute;nchez - alberto.lorente92@gmail.com</h5>
-			<h5 align="center">All images created by Javier Arnanz - javiermoka@gmail.com</h5>
-			<br/>
-		</div>
-	</section>
-</body>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    <script src="assets/js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
--- tfg.php
+++ tfg.php
@@ -1,96 +1,109 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Alberto Lorente. Software Engineer</title>
-	<link href="css/style.css" rel="stylesheet" type="text/css" />
-	<script type="text/javascript" src="js/javascript.js"></script>
-	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-	<link rel="shortcut icon" href="img/icon.png" type="image/png">
-</head>
-<body onload="javascript:window_onload();">
-	<header class="header" id="home">	
-		<!-- NAVIGATION LINKS -->
-		<div class="navbar_absolute" id="menu">
-			<br/>
-			<table>
-				<tr>
-					<td>
-						<a class="navbar" href="index.php"><img src="/img/ALlogo.png" alt="Logo AlbertoLorente" width="400" height="65"></a>
-					</td>
-					<td id="entryMenu">
-						<a href="index.php#home">Home</a>
-					</td>
-					<td id="entryMenu">
-						<a href="index.php#studies">Studies</a>
-					</td>
-					<td id="entryMenu">
-						<a href="index.php#workExperience">Work Experience</a>
-					</td>
-					<td id="entryMenu">
-						<a href="index.php#recommendations">Recommendations</a>
-					</td>
-					<td id="entryMenu">
-						<a href="index.php#jobs">Jobs</a>
-					</td>
-					<td id="entryMenu">
-						<a href="index.php#personal">Personal</a>
-					</td>
-				</tr>
-			</table>
-		</div>
-		
-	<section>
-		<div class="textCenter">  
-			<br/>
-			<h1>Final Degree Work</h1>
-			<h5>StarCraft solving mazes using reinforcement learning</h5>
-			<p><strong>Abstract</strong></p>
-			<p>In this project the team will try to develop an application that, using the <strong>Q-Learning algorithm</strong>, 
-			will be able to learn several activities by itself in the environment of real-time strategy 
-			game (RTS) StarCraft. In particular, we will use it to solve maze on an autonomous way, 
-			in other words, without providing it with previous knowledge about the maze's structure. </p>
-			<p>To accomplish it, we will be moving one game-unity inside a maze that may have 
-			enemies, and thanks to the reinforcement learning the game-unity will be able to find the 
-			maze's exit by itself. </p>
-			<p>We will show experiments with different learning strategies, using diverse rewards, and 
-			at the end we will explain which of them worked better. Also we will develop a reusable 
-			framework for this kind of learning based on Q-Learning algorithm, which will be used to 
-			solve our problem and could be useful as a base to future developments.</p>
-			<p>This project was realized with the help of my two friends Juan Deltell and Jes&uacutes Mart&iacutenez.</p>
-			<p>If you want to read all our work you could do it <a href="https://drive.google.com/file/d/0B1MxBhOQaDc-eXZiZlM2V1FWQ0E/view?usp=sharing" target="_blank">here</a>.</p>
-			<p align="middle"><iframe width="560" height="315" src="https://www.youtube.com/embed/tYYGrzDSRLw" frameborder="0" allowfullscreen></iframe></p>
-		</div>
-	</section>
-	
-	<section class="blueEnd" id="social">
-		<div class="blueEnd">
-			<table>
-				<tr>
-					<td>
-						<a href="mailto:alberto.lorente92@gmail.com"><img src="/img/gmail.jpg" alt="Gmail alberto.lorente92@gmail.com" width="100" height="100"></a>
-					</td>
-					<td>
-						<a href="https://github.com/AlbertoLorente92" target="_blank"><img src="/img/github.png" alt="GitHub AlbertoLorente92" width="100" height="100"></a>
-					</td>
-				</tr>
-			</table>
-			<br/>
-		</div>
-	</section>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
 
-	<section class="grayEnd" id="agreement">
-		<div class="">  
-			<p align="center"><a class="navbar" href="index.php"><img src="/img/ALlogo.png" alt="Logo AlbertoLorente" width="400" height="65" align="middle"></a></p>
-		</div>
-	</section>
-	
-	<section class="blueEnd" id="foot">
-		<div class="">  
-			<br/>
-			<h5 align="center">Alberto Lorente S&aacute;nchez - alberto.lorente92@gmail.com</h5>
-			<h5 align="center">All images created by Javier Arnanz - javiermoka@gmail.com</h5>
-			<br/>
-		</div>
-	</section>
-</body>
+    <title>Alberto Lorente. Software Engineer</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/theme.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body role="document">
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="index.php"><img src="img/new.png" alt="AlbertoLorente" border="2" align="middle" width="300px" height="50px"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home</a></li>
+            <li><a href="index.php#studies">Studies</a></li>
+            <li><a href="index.php#workExperience">Work Experience</a></li>
+            <li><a href="index.php#recommendations">Recommendations</a></li>
+			<li><a href="index.php#jobs">Jobs</a></li>
+			<li><a href="index.php#personal">Personal</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container theme-showcase" role="main">
+
+      <div class="page-header">
+        <h1>Final Degree Work</h1>
+		<h3>StarCraft solving mazes using reinforcement learning</h3>
+      </div>
+
+		
+		<p><strong>Abstract</strong></p>
+		<p>In this project the team will try to develop an application that, using the <strong>Q-Learning algorithm</strong>, 
+		will be able to learn several activities by itself in the environment of real-time strategy 
+		game (RTS) StarCraft. In particular, we will use it to solve maze on an autonomous way, 
+		in other words, without providing it with previous knowledge about the maze's structure. </p>
+		<p>To accomplish it, we will be moving one game-unity inside a maze that may have 
+		enemies, and thanks to the reinforcement learning the game-unity will be able to find the 
+		maze's exit by itself. </p>
+		<p>We will show experiments with different learning strategies, using diverse rewards, and 
+		at the end we will explain which of them worked better. Also we will develop a reusable 
+		framework for this kind of learning based on Q-Learning algorithm, which will be used to 
+		solve our problem and could be useful as a base to future developments.</p>
+		<p>This project was realized with the help of my two friends Juan Deltell and Jes&uacutes Mart&iacutenez.</p>
+		<p>If you want to read all our work you could do it <a href="http://eprints.ucm.es/32886/1/Memoria%20TFG%20-%20Resoluci%C3%B3n%20de%20laberintos%20en%20StarCraft%20empleando%20aprendizaje%20por%20refuerzo.pdf" target="_blank">here</a>.</p>
+		<p align="middle"><iframe width="560" height="315" src="https://www.youtube.com/embed/tYYGrzDSRLw" frameborder="0" allowfullscreen></iframe></p>
+
+	  <footer class="blog-footer">
+		<a href="mailto:alberto.lorente92@gmail.com"><img src="img/gmail.png" alt="Gmail alberto.lorente92@gmail.com" width="100" height="100"></a>
+		<a href="https://github.com/AlbertoLorente92" target="_blank"><img src="img/github.png" alt="GitHub AlbertoLorente92" width="100" height="100"></a>
+	    <p>
+	      <a href="#">Back to top</a>
+	    </p>
+	  </footer>
+		
+
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="dist/js/bootstrap.min.js"></script>
+    <script src="assets/js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
